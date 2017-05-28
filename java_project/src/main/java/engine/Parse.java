6da0c76e7115c696f51ab3d4a880a96d49d01d55
Salete Teixeira <a75281@alunos.uminho.pt>
2017-05-28 22:58:05 +0100

    parse

--- java_project/src/main/java/engine/Parse.java
+++ java_project/src/main/java/engine/Parse.java
@@ -1,5 +1,18 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 
 /**
  * Classe que retira as informações dos snapshots e as insere nas estruturas criadas.
